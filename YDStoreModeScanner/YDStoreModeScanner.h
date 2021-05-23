33eaa8c75e1672d2f6ff9140046fc3665db3d9e5
Douglas Hennrich <douglashennrich@yourdev.com.br>
2021-05-23 13:28:17 -0300

    First commit

---
+++ YDStoreModeScanner/YDStoreModeScanner.h
@@ -0,0 +1,18 @@
+//
+//  YDStoreModeScanner.h
+//  YDStoreModeScanner
+//
+//  Created by Douglas Hennrich on 23/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YDStoreModeScanner.
+FOUNDATION_EXPORT double YDStoreModeScannerVersionNumber;
+
+//! Project version string for YDStoreModeScanner.
+FOUNDATION_EXPORT const unsigned char YDStoreModeScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YDStoreModeScanner/PublicHeader.h>
+
+
